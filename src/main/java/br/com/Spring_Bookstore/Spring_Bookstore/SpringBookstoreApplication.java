7f5aea59b2d8a2d6cdda460719811301640ab887
Vitor ozorio <vitormanoelbasilio357@gmail.com>
2024-12-16 12:31:53 -0300

    Enviando os primeiros arquivos

---
+++ src/main/java/br/com/Spring_Bookstore/Spring_Bookstore/SpringBookstoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.Spring_Bookstore.Spring_Bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/Spring_Bookstore/Spring_Bookstore/SpringBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Spring_Bookstore.Spring_Bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
