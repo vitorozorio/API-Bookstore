6e46eeb7632e4dacf30c0ef19129048ad8d0ea4b
vitorozorio <vitormanoelbasilio357@gmail.com>
2025-02-18 06:08:40 -0300

    primeiras classes do sistema

---
+++ src/main/java/br/com/Spring_Bookstore/Spring_Bookstore/domain/Person.java
@@ -0,0 +1,32 @@
+package br.com.Spring_Bookstore.Spring_Bookstore.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collection="Person")
+public class Person implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+
+    private String userName;
+    private String password;
+    private String email;
+    private Integer level;
+
+    @DBRef(lazy = true)
+    private List<Book> books = new ArrayList<>();
+
+
+
+
+
+}
